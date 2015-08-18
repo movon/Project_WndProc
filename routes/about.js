6fc1bc90a5365148ff3fd677662403c4a909ccd0
talbor49 <talbor49@gmail.com>
2015-08-18 21:50:45 +0300

    Removed content fix

--- routes/about.js
+++ routes/about.js
@@ -5,9 +5,7 @@ var router = express.Router();
 router.get('/', function (req, res) {
     'use strict';
     var extra = getExtra(req);
-    var content = 'This way you will learn a lot about assembly language and low-level programming,' +
-        'how the operating system handles graphics, and how the computer works.';
-    res.render('about', { title: 'About', extra: extra, username: req.session.username, content: content});
+    res.render('about', { title: 'About', extra: extra, username: req.session.username});
 });
 
 

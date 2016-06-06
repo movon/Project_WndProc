d8656d017f0276ce0034cebe7e7f589fdd6f47a3
talbor49 <talbor49@gmail.com>
2016-06-06 00:53:29 -0700

    Found bug in guides - filepath was wrong in unix based systems.

--- routes/guides.js
+++ routes/guides.js
@@ -7,15 +7,7 @@ router.get('/', function (req, res) {
     'use strict';
     var extra = getExtra(req);
     var guideName = 'guide' + req.baseUrl.slice(req.baseUrl.lastIndexOf("/") + 1);
-    var fileName = __dirname + '\\..\\views\\' + guideName;
-    console.log(fileName);
-    fs.exists(fileName + '.ejs', function(exists) {
-        if (exists) {
-            res.render(guideName, { title: 'Guide', extra: extra, username: req.session.username});
-        } else {
-            res.render('guides', { title: 'Guides', extra: extra, username: req.session.username});
-        }
-    });
+    res.render(guideName, { title: 'Guide', extra: extra, username: req.session.username});
 });
 
 module.exports = router;

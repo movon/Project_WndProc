be55066b00ce51b9f8ed5dc7a4158384b5c6dba5
Daniel Eliad <daniel.eliad32@gmail.com>
2018-10-13 19:02:37 +0300

    updated the rest of the routes to the global client

--- routes/changepassword.js
+++ routes/changepassword.js
@@ -1,67 +1,48 @@
 var express = require('express');
 var router = express.Router();
-var crypto = require('crypto');
-var { Client } = require('pg');
-const client = new Client(
-    {
-        connectionString: process.env.DATABASE_URL
-    }
-);
 
 
 
 router.post('/', function(req, res) {
     'use strict';
     console.log("Got request to change password");
-    client.connect()
-        .then(() => {
-            var username = req.session.username;
-            var extra = getExtra(req);
+        var username = req.session.username;
+        var extra = getExtra(req);
 
-            if (req.body.password !== req.body.verifypassword) {
-                client.end();
-                res.render('userpanel', { title: 'User Panel', extra:extra, username:req.session.username, error:"Passwords do not match"});
+        if (req.body.password !== req.body.verifypassword) {
+            res.render('userpanel', { title: 'User Panel', extra:extra, username:req.session.username, error:"Passwords do not match"});
+            return;
+        }
+
+        connection.query("select * from users where username=$1;", [username]).then((rows) => {
+            var oldpass = req.body.old;
+            var salt = rows[0].salt;
+            var saltpassword = oldpass + salt;
+            var hashedpassword = crypto.createHash('md5').update(saltpassword).digest('hex');
+
+            if (rows[0].hashed !== hashedpassword) {
+                res.render('userpanel', { title: 'User Panel', extra:extra, username:req.session.username, error:"Invalid old password"});
                 return;
             }
 
-            connection.query("select * from users where username=$1;", [username]).then((rows) => {
-                var oldpass = req.body.old;
-                var salt = rows[0].salt;
-                var saltpassword = oldpass + salt;
-                var hashedpassword = crypto.createHash('md5').update(saltpassword).digest('hex');
+            var newpass = req.body.password;
+            salt = rows[0].salt;
+            saltpassword = newpass + salt;
+            hashedpassword = crypto.createHash('md5').update(saltpassword).digest('hex');
 
-                if (rows[0].hashed !== hashedpassword) {
-                    client.end();
-                    res.render('userpanel', { title: 'User Panel', extra:extra, username:req.session.username, error:"Invalid old password"});
-                    return;
-                }
-
-                var newpass = req.body.password;
-                salt = rows[0].salt;
-                saltpassword = newpass + salt;
-                hashedpassword = crypto.createHash('md5').update(saltpassword).digest('hex');
-
-                connection.query("UPDATE users SET hashed=$1 where username=$2;", [hashedpassword, username]).then(
-                    (result) => {
-                        console.log("updated user: ", ussername, "password successfuly");
-                        client.end();
-                        res.render('userpanel', { title: 'User Panel', extra:extra, username: req.session.username, error: "Password change success." });
-                    },
-                    (err) => {
-                        console.error("Error occured in changing passwords:", err);
-                        client.end();
-                        res.render('userpanel', { title: 'User Panel', extra:extra, username:req.session.username, error:"Error updating password"});
-                    });
-                }, (err) => {
-                console.err("Error querying database for changing password", err, err.stack);
-                client.end();
-                res.render('userpanel', { title: 'User Panel', extra:extra, username:req.session.username, error:"Error querying database for username"});
-            });
-    }, (err) => {
-            console.error('connection errror when changing password', err.stack);
-            res.json({"code": 100, "status": "Error in connection database"});
+            connection.query("UPDATE users SET hashed=$1 where username=$2;", [hashedpassword, username]).then(
+                (result) => {
+                    console.log("updated user: ", ussername, "password successfuly");
+                    res.render('userpanel', { title: 'User Panel', extra:extra, username: req.session.username, error: "Password change success." });
+                },
+                (err) => {
+                    console.error("Error occured in changing passwords:", err);
+                    res.render('userpanel', { title: 'User Panel', extra:extra, username:req.session.username, error:"Error updating password"});
+                });
+            }, (err) => {
+            console.err("Error querying database for changing password", err, err.stack);
+            res.render('userpanel', { title: 'User Panel', extra:extra, username:req.session.username, error:"Error querying database for username"});
         });
-
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
-const { Client } = require('pg');
-const client = new Client(
-    {
-        connectionString: process.env.DATABASE_URL
-    }
-);
 
 /* GET login page. */
 router.get('/', function (req, res) {
@@ -18,49 +12,39 @@ router.get('/', function (req, res) {
 
 router.post('/', function (req, res) {
     'use strict';
-    client.connect()
-        .then(() => {
-            var extra = getExtra(req);
-            var username = req.body.username;
-            var password = req.body.password;
-            client.query("select * from users where username=$1;", [username])
-                .then(
-                    (rows) => {
-                        if (rows.rowCount === 0) {
-                            console.log("user not found");
-                            client.end();
-                            res.render('login', {title: 'Invalid username or password!', extra:extra, username:req.session.username});
-                            return;
-                        }
-
-                        // verify password hash
-                        var salt = rows.rows[0].salt;
-                        var saltpassword =  password + salt;
-                        var hashedpassword = crypto.createHash('md5').update(saltpassword).digest('hex');
-
-                        if (rows.rows[0].hashed !== hashedpassword) {
-                            console.log("password hash doesn't match given password");
-                            client.end();
-                            res.render('login', {title: 'Invalid username or password!', extra: extra, username: req.session.username});
-                            return;
-                        }
-
-                        req.session.username = rows.rows[0].username;
-                        req.session.privileges = (rows.rows[0].privileges);
-                        console.log("login successful");
-                        client.end();
-                        res.redirect('/');
-                    },
-                    (err) => {
-                        console.error("failed querying users database for logging in", err, err.stack);
-                        client.end();
-                        res.render('login', {title: 'Failed querying database for logging in', extra: extra, username: req.session.username});
+        var extra = getExtra(req);
+        var username = req.body.username;
+        var password = req.body.password;
+        client.query("select * from users where username=$1;", [username])
+            .then(
+                (rows) => {
+                    if (rows.rowCount === 0) {
+                        console.log("user not found");
+                        res.render('login', {title: 'Invalid username or password!', extra:extra, username:req.session.username});
+                        return;
                     }
-                );
-        }, (err) => {
-            console.error('connection errror when accessing admin panel', err.stack);
-            res.json({"code": 100, "status": "Error in connection database"});
-        });
+
+                    // verify password hash
+                    var salt = rows.rows[0].salt;
+                    var saltpassword =  password + salt;
+                    var hashedpassword = crypto.createHash('md5').update(saltpassword).digest('hex');
+
+                    if (rows.rows[0].hashed !== hashedpassword) {
+                        console.log("password hash doesn't match given password");
+                        res.render('login', {title: 'Invalid username or password!', extra: extra, username: req.session.username});
+                        return;
+                    }
+
+                    req.session.username = rows.rows[0].username;
+                    req.session.privileges = (rows.rows[0].privileges);
+                    console.log("login successful");
+                    res.redirect('/');
+                },
+                (err) => {
+                    console.error("failed querying users database for logging in", err, err.stack);
+                    res.render('login', {title: 'Failed querying database for logging in', extra: extra, username: req.session.username});
+                }
+            );
 });
 
 module.exports = router;
